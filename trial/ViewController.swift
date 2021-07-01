cc4e44f20ce8ef35c96bbb7414c202d24379fa35
nethra <nethra_raghu@s.thevillageschool.com>
2021-07-01 14:47:02 -0500

    fixed resource page

--- trial/ViewController.swift
+++ trial/ViewController.swift
@@ -4,14 +4,27 @@
 //
 //  Created by Nethra Raghu on 7/1/21.
 //
-
+import AVKit
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "Smiling Mind", ofType: "mp4")!))
+        
+        let layer = AVPlayerLayer(player:player)
+        layer.frame = view.bounds
+        view.layer.addSublayer(layer)
+        
+        player.play()
     }
 
 
